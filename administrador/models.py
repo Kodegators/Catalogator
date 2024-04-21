37a7d95e0203b0046a55c9526d6ef3599a3da653
Daniel Rodrigues <daniel97rf@gmail.com>
2024-04-21 15:58:30 -0300

    criação dos projetos e aplicações

---
+++ administrador/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Generos(models.Model):
+    genero = models.CharField('Nome', max_length=100)
+
+class Itens(models.Model):
+    nome = models.CharField('Nome', max_length=200)
+    genero = models.ForeignKey(Generos, on_delete=models.PROTECT)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=8)
